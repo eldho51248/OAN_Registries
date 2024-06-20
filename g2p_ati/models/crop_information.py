7dd1014599c5e38688a93097cfb4740e970402a8
EyuaelB <eyuaelbezabeh@gmail.com>
2024-06-20 19:06:51 +0300

    new fields update

--- g2p_ati/models/crop_information.py
+++ g2p_ati/models/crop_information.py
@@ -1,14 +1,16 @@
 from odoo import fields, models
 
 
+
 class G2PCropInformation(models.Model):
     _name = "g2p.crop.information"
 
     partner_id = fields.Many2one("res.partner", string="partner")
-    crop = fields.Many2one("g2p.crop")
-    variety = fields.Many2one("g2p.crop.variety", string="Crop Variety")
-    illness_type = fields.Many2many("g2p.crop.illness.type")
-    illness_images = fields.Many2many("ir.attachment")
+    crop = fields.Many2one("g2p.crop", string='Crop')
+    illness_type = fields.Many2many("g2p.crop.illness.type", string="Disease")
+
+    
+    # illness_images = fields.Many2many("ir.attachment")
 
 
 class G2PCropIllnessType(models.Model):
--- g2p_ati/models/farmer.py
+++ g2p_ati/models/farmer.py
@@ -1,47 +1,206 @@
 from odoo import fields, models
 
 
+class Region(models.Model):
+    _name = 'g2p.region'
+    
+    code = fields.Char(string="Code")
+    
+
+class Zone(models.Model):
+    _name = 'g2p.zone'
+    
+    region = fields.Many2one("g2p.region", string="Region")
+    code = fields.Char(string="Code")
+
+
+
+class Woreda(models.Model):
+    _name = 'g2p.zone'
+    
+    zone = fields.Many2one("g2p.zone", string="Zone")
+    code = fields.Char(string="Code")
+
+
+class Kebele(models.Model):
+    _name = 'g2p.zone'
+    woreda = fields.Many2one("g2p.woreda", string="Woreda")
+    code = fields.Char(string="Code")
+
+
+
+
+
+class G2PPrimaryCooperative(models.Model):
+    _name = 'g2p.primary.cooperative'
+    _description = 'Primary Cooperative'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+
+
+class G2PCooperativeUnion(models.Model):
+    _name = 'g2p.cooperative.union'
+    _description = 'Cooperative Union'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+
+class G2PPrimaryCommodity(models.Model):
+    _name = 'g2p.primary.commodity'
+    _description = 'Primary Commodity'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+
+
+
+
+class G2PWaterSource(models.Model):
+    _name = 'g2p.water.source'
+    _description = 'Water Source'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+    
+
+class G2PMachinery(models.Model):
+    _name = 'g2p.machinery'
+    _description = 'Machinery'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(
+        string='Name',
+        required=True,
+    )
+
+#TODO add type to phone number and logic
+
+
 class G2PFarmer(models.Model):
     _inherit = "res.partner"
+    
+    
+    # Basic Information
+    
+    region = fields.Many2one("g2p.region", string="Region")
+    zone = fields.Many2one("g2p.zone", string="Zone")
+    woreda = fields.Many2one("g2p.woreda", string="Woreda")
+    kebele = fields.Many2one("g2p.kebele", string="Kebele")
+    
+    
+    given_name = fields.Char(string="First Name(Eng)", translate=False)
+    family_name = fields.Char(string="Father Name(Eng)", translate=False)
+    gf_name_eng = fields.Char(string="Grand Father Name(Eng)", translate=False)
+    
+    first_name_amh = fields.Char(string="First Name(Amh)", translate=False)
+    family_name_amh = fields.Char(string="Father Name(Amh)", translate=False)
+    gf_name_amh = fields.Char(string="Grand Father Name(Amh)", translate=False)
+    
+    first_name_oro = fields.Char(string="First Name(Oro)", translate=False)
+    family_name_oro = fields.Char(string="Father Name(Oro)", translate=False)
+    gf_name_amh_oro = fields.Char(string="Grand Father Name(Oro)", translate=False)
+    
+    birthdate = fields.Date(string="Date Of Birth(EC)")
+    birthdate_gc = fields.Date(string="Date Of Birth(GC)")
+    primary_Language = fields.Many2one("res.lang", string="Primary language")
+    is_farmer = fields.Boolean("Are you a Farmer? ")
+    farming_type = fields.Selection(string="farming Type", selection=[
+        ('agro', 'Agro-Pastorial'),
+        ('pastorial', 'Pastorial'),
+        ('mixed', 'Mixed farming'),
 
-    grand_father_name = fields.Char(translate=False)
-    hh_size = fields.Integer(string="Household Size")
-    hh_income = fields.Float(string="House Hold Income")  # might also change to Monetary if needed
-    ed_level = fields.Selection(
-        [
-            ("pr", "Primary School"),
-            ("ss", "Secondary School"),
-            ("tvs", "Technical and Vocation Sciences"),
-            ("col", "College"),
-            ("unv", "University"),
+    ])
+
+    
+    # Memebership
+    
+    is_member_of_primary_cooperative = fields.Boolean(string="Is Member Of Primary Cooperative? ")
+    primary_cooperatives = fields.Many2many('g2p.primary.cooperative', string="Primary Cooperatives" )
+
+    is_member_of_cooperative_union = fields.Boolean(string="Is Member Of Cooperative Union? ")
+    cooperative_unions = fields.Many2many('g2p.cooperative.union', string="Cooperative Unions" )
+    
+    is_member_in_farmer_cluster = fields.Boolean(string="Is Member In Farmer Cluster? ")
+    primary_commodity = fields.Many2one('g2p.primary.commodity', string="Primary Commodity")
+    role_in_farmer_cluster = fields.Selection(string="Role In Farmer Cluster", selection=[
+        ('lead', 'Lead'),
+        ('deputy','Deputy'),
+        ('secretary', 'Secretary'),
+        ('accountant', 'Accounatnt'),
+        ('member', 'Member')
+    ])
+    
+    
+    # Agricultural Input
+    amount_fertilizer_utilized = fields.Float(string="What is The amount Of fertilizer you have(qt)? ")
+    amount_pesticide_utilized = fields.Float(string="What is The amount Of pesticide you have(L)? ")
+    amount_insecticide_utilized = fields.Float(string="What is The amount Of insecticide you have in(L)? ")
+    amount_improved_seed_utilized = fields.Float(string="What is The amount Of improved seed you have used(qt)? ")
+    
+    
+    # Access To resources
+    water_resources = fields.Many2many('g2p.water.source', string="What Water Sources do you use?")
+    access_to_machinery = fields.Boolean(string="Do you use machinery? ")
+    type_of_machinery =fields.Many2many('g2p.machinery', string='What kind of machinery do you use? ')
+    
+    
+    # Acces To Finance    
+    # type_of_machinery =fields.Many2many('g2p.machinery',string='What kind of Machinery do you use? ')
+    # fund_received_type = fields.Selection(selection=[("cash", "Cash"), ("inkind", "In Kind")])
+    
+    no_finace_access = fields.Boolean("No finance access")
+    loans = fields.Boolean("Loans")
+    insurance = fields.Boolean("Insurance")
+    savings = fields.Boolean("Savings")
+    
+    other_farmer_in_hh = fields.Boolean('Is there any other farmer in the household who has separate land?')
+    
+    # Socio economic Data
+    
+    martial_status = fields.Selection(
+        string='Martial Status',
+        selection=[
+            ('single', 'Single'), 
+            ('married', 'Married'), 
+            ('divorced', 'Divorced'), 
+            ('widowed', 'Widowed')]
+    )
+    
+    education = fields.Selection(
+        [ ("illeterate", "Illeterate"),
+            ("readwrite", "Can Read and Write"),
+            ("basic", "Basic(1-8)"),
+            ("intermediary", "Intermediary(9-12)"),
+            ("technic", "Vocational and Technical School"),
+            ("higher", "Higher education(Unversity, College)"),
         ],
         string="Educational Level",
     )
-
-    farming_type = fields.Many2one("g2p.farming.type")
-
-    # Farmer Details
-    # Membership
-
-    is_member_of_primary_cooperative = fields.Boolean(string="Is Member Of Primary Cooperative? ")
-    is_member_of_union = fields.Boolean(string="Is Member In Union? ")
-    is_member_in_farmer_cluster = fields.Boolean(string="Is Member In Farmer Cluster? ")
-
-    # Agricultural Input
-    amount_fertilizer_utilized = fields.Float(string="Amount Of fertilizer Utilized in kg")
-    amount_pesticide_utilized = fields.Float(string="Amount Of Pesticide Utilized in kg")
-    amount_insecticide_utilized = fields.Float(string="Amount Of Insecticide Utilized in kg")
-    amount_improved_seed_utilized = fields.Float(string="Amount Of Improved Seed Utilized in kg")
-
-    # Financial Services
-    fund_received_type = fields.Selection(selection=[("cash", "Cash"), ("inkind", "In Kind")])
-    loans = fields.Boolean()
-    insurance = fields.Boolean()
-    savings = fields.Boolean()
-
-    # Access To resources
-    access_to_water = fields.Boolean()
-    access_to_machinery = fields.Boolean()
+    hh_income = fields.Float(string='Total Amount')
+    hh_size = fields.Integer(string="Household Size")
 
     # Land Informations
     land_information_ids = fields.One2many("g2p.land.information", "partner_id", string="Land Information")
@@ -49,3 +208,4 @@ class G2PFarmer(models.Model):
     livestock_information_ids = fields.One2many(
         "g2p.livestock.information", "partner_id", string="Live Stock Information"
     )
+    
