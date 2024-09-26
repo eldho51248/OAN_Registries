6f5b8190d1dc54c5af1448dc97d4f16c346be42a
DagmawitGT <dagmawitgetachewteffera@gmail.com>
2024-09-26 08:45:56 +0300

    handled merge conflicts

--- g2p_ati_service_provider_portal/controllers/main.py
+++ g2p_ati_service_provider_portal/controllers/main.py
@@ -2382,7 +2382,7 @@ class AtiserviceProviderBeneficiaryManagement(G2PServiceProviderBeneficiaryManag
                         continue
                     else:
                         kind_name = membership.kind.name if membership.kind else None
-                       
+
                         member_list.append(
                             {
                                 "id": membership.individual.id,
