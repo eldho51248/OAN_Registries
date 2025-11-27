79533de7f68e7858427fa10b1f38e8397fa11b37
Rediet-Ferew <redietasnakech@gmail.com>
2025-11-27 20:55:27 +0300

    fix: move menu items to setting parent menu

--- g2p_ati_integrations/__manifest__.py
+++ g2p_ati_integrations/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "tools",
     "description": """
 """,
-    "depends": ["g2p_draft_publish", "g2p_ati", "g2p_leaflet_map"],
+    "depends": ["g2p_draft_publish", "g2p_ati", "g2p_leaflet_map", "base"],
     "data": [
         "security/rules.xml",
         "security/ir.model.access.csv",
