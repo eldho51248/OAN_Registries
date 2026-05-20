5b9332983ba023c722a0aa61cea6cd104835aa41
rithikharavi-ai <rithikharavi@gmail.com>
2026-05-20 12:52:49 +0530

    Update crop_registry.py

--- g2p_crop_registry/model/crop_registry.py
+++ g2p_crop_registry/model/crop_registry.py
@@ -8,7 +8,7 @@ class G2PCrop(models.Model):
     _description = 'G2p Crop Registry'
 
     farmer_id = fields.Char(string="Farmer ID")
-    fyda_id = fields.Char(string="Fyada ID")
+    fyda_id = fields.Char(string="Fayda ID")
     farmer_display_id = fields.Char(string='Farmer Name')
     zone_name_id = fields.Many2one('g2p.zone',
         string='Zone Name',
